0efc66e0c2e62777fb4b12c6197390430a0f5799
Akandesh <Akandessh@live.com>
2021-09-16 16:31:34 +0200

    Initial commit

---
+++ snake/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace snake
+{
+    class Program
+    {
+        void main( ) {
+            const int gameWidth = 80;
+            const int gameHeight = 20;
+            var snake = new Snake( gameWidth, gameHeight ); ;
+
+            setupGame( gameWidth, gameHeight );
+            return;
+            while ( true ) {
+                snake.Tick( );
+                Thread.Sleep( 1000 / 15 );
+            }
+        }
+
+
+        void setupGame( int width, int height ) {
+            Console.Title = "Snake";
+            drawBorder( width, height );
+            
+        }
+
+        void drawBorder( int width, int height ) {
+            Console.Clear( );
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            for ( int y = 0; y <= height; y++ ) {
+                bool onTop = y == 0;
+                bool onBottom = y == height;
+                for ( int x = 0; x <= width; x++ ) {
+                    bool onEdge = ( onTop && x == 0 ) ||
+                                  ( onTop && x == width ) ||
+                                  ( onBottom && x == 0 ) ||
+                                  ( onBottom && x == width );
+                    if ( onEdge ) {
+                        Console.Write( '+' );
+                        continue;
+                    }
+
+                    if ( x == 0 || x == width || onTop || onBottom ) {
+                        Console.Write( '#' );
+                        continue;
+                    }
+
+                    Console.Write( ' ' );
+                }
+                Console.WriteLine( );
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static void Main( ) {
+            new Program( ).main( ); // silly static escape
+        }
+    }
+}
---
+++ snake/Snake.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    struct Coordinate
+    {
+        public int x;
+        public int y;
+        public static Coordinate operator +(Coordinate a, Coordinate b) {
+            a.x += b.x;
+            a.y += b.y;
+            return a;
+        }
+
+        public static bool operator ==(Coordinate a, Coordinate b) {
+            return a.x == b.x && a.y == b.y;
+        }
+        public static bool operator !=(Coordinate a, Coordinate b) {
+            return !(a == b);
+        }
+    }
+
+    class Snake
+    {
+        private List<Coordinate> _snakeGrids; // grids where the snake is currently inside
+        private List<Coordinate> _drawnGrids; // last grids that were drawn, used to make sure i don't have any leftovers
+        private int _snakeLength; // snakeGrids.Length essentially but used for trimming
+
+        private Coordinate _currentAcceleration;
+        private Coordinate _currentHeadPosition;
+        public Snake(int gameWidth, int gameHeight) {
+            _snakeLength = 3;
+        }
+
+        public void Tick() {
+            _currentHeadPosition += _currentAcceleration;
+
+
+            DrawSnake();
+        }
+
+        private void DrawSnake() {
+
+            foreach (var drawnGrid in _drawnGrids)
+            {
+                if (_snakeGrids.Exists(snakeGrid => snakeGrid == drawnGrid)) // still valid
+                    continue;
+
+                Console.SetCursorPosition(drawnGrid.x + 1, drawnGrid.y + 1);
+                Console.Write(" ");
+            }
+
+            foreach (var snakeGrid in _snakeGrids) {
+                if (_drawnGrids.Exists(drawnGrid => drawnGrid == snakeGrid)) // already drawn
+                    continue;
+
+                Console.SetCursorPosition(snakeGrid.x + 1, snakeGrid.y + 1);
+                Console.Write("#");
+            }
+            _drawnGrids = _snakeGrids; // now all snakes are drawn :)
+        }
+    }
+}
